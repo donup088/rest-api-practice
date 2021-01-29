ac490158469c44096ae5af7ca1218e9a56385b47
donup3 <donup08@naver.com>
2021-01-29 17:34:49 +0900

    Event 생성시 필드에 없는 입력값이 들어오면 BadRequest 발생하도록 구현

--- src/test/java/study/restapi/events/EventControllerTest.java
+++ src/test/java/study/restapi/events/EventControllerTest.java
@@ -35,6 +35,36 @@ public class EventControllerTest {
 
     @Test
     public void createEvent() throws Exception {
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .description("Rest API")
+                .beginEventDateTime(LocalDateTime.of(2020, 1, 27, 16, 3))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020, 1, 28, 12, 1))
+                .beginEventDateTime(LocalDateTime.of(2020, 1, 27, 12, 1))
+                .endEventDateTime(LocalDateTime.of(2020, 1, 28, 12, 1))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역")
+                .build();
+
+        mvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(eventDto)))// 이 응답을 받고 싶다.
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DREFT.name()));
+
+    }
+
+    @Test
+    public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100L)
                 .name("Spring")
@@ -57,13 +87,7 @@ public class EventControllerTest {
                 .accept(MediaTypes.HAL_JSON)
                 .content(objectMapper.writeValueAsString(event)))// 이 응답을 받고 싶다.
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-                .andExpect(jsonPath("id").value(Matchers.not(100)))
-                .andExpect(jsonPath("free").value(Matchers.not(true)))
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DREFT.name()));
+                .andExpect(status().isBadRequest());
 
     }
 }
