37b6ae24924f24df90b67485d05d4860cc302d0a
donup3 <donup08@naver.com>
2021-01-29 18:49:49 +0900

    Errors를 body에 넣어 보낼 수 있도록 구현

--- src/main/java/study/restapi/events/EventValidator.java
+++ src/main/java/study/restapi/events/EventValidator.java
@@ -8,17 +8,19 @@ import java.time.LocalDateTime;
 
 @Component
 public class EventValidator {
-    public void validate(EventDto eventDto, Errors errors){
-        if(eventDto.getBasePrice()>eventDto.getBasePrice()&&eventDto.getMaxPrice()!=0){
-            errors.rejectValue("basePrice","wrongValue","BasePrice is Wrong");
-            errors.rejectValue("maxPrice","wrongValue","MaxPrice is Wrong");
+    public void validate(EventDto eventDto, Errors errors) {
+        if (eventDto.getBasePrice() > eventDto.getBasePrice() && eventDto.getMaxPrice() != 0) {
+            //필드 에러
+            errors.rejectValue("basePrice", "wrongValue", "BasePrice is Wrong");
+            errors.rejectValue("maxPrice", "wrongValue", "MaxPrice is Wrong");
+            //글로벌 에러
+            errors.reject("wrongPrice", "prices is Wrong");
         }
         LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
-        if(endEventDateTime.isBefore(eventDto.getBeginEventDateTime())||
-        endEventDateTime.isBefore(eventDto.getCloseEnrollmentDateTime())||
-        endEventDateTime.isBefore(eventDto.getBeginEnrollmentDateTime())){
-            errors.rejectValue("endEventDateTime","wrongValue","EndEventDateTime is Wrong");
+        if (endEventDateTime.isBefore(eventDto.getBeginEventDateTime()) ||
+                endEventDateTime.isBefore(eventDto.getCloseEnrollmentDateTime()) ||
+                endEventDateTime.isBefore(eventDto.getBeginEnrollmentDateTime())) {
+            errors.rejectValue("endEventDateTime", "wrongValue", "EndEventDateTime is Wrong");
         }
-
     }
 }
--- src/test/java/study/restapi/events/EventControllerTest.java
+++ src/test/java/study/restapi/events/EventControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import study.restapi.common.TestDescription;
 
 import java.time.LocalDateTime;
 
@@ -30,6 +31,7 @@ public class EventControllerTest {
     ObjectMapper objectMapper;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
@@ -60,6 +62,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우 에러가 발생하는 테스트")
     public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100L)
@@ -88,6 +91,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력값이 비어 있는 경우 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
@@ -98,7 +102,7 @@ public class EventControllerTest {
     }
 
     @Test
-
+    @TestDescription("입력값이 잘못된 경우 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
@@ -116,6 +120,10 @@ public class EventControllerTest {
         mvc.perform(post("/api/events")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(eventDto)))
-                .andExpect(status().isBadRequest());
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[0].objectName").exists())
+                .andExpect(jsonPath("$[0].defaultMessage").exists())
+                .andExpect(jsonPath("$[0].code").exists());
     }
 }
